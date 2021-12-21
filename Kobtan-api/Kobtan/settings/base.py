256ead4b97b6a2baaaa04e8c9295633651c9a49b
moha elbuni <elbunim@gmail.com>
2021-12-21 10:45:07 +0200

    deplyment setup

--- Kobtan-api/Kobtan/settings/base.py
+++ Kobtan-api/Kobtan/settings/base.py
@@ -9,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%ok7m7e202=g9_*s!^vo*0m!m3ay!o1f9*z1f%)8n11pl(fg2v'
+# SECRET_KEY = 'django-insecure-%ok7m7e202=g9_*s!^vo*0m!m3ay!o1f9*z1f%)8n11pl(fg2v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
