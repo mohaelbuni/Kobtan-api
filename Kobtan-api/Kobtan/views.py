2f2b1fe633ac825b10577e49b26194e2218c5e55
moha elbuni <elbunim@gmail.com>
2021-12-21 14:13:37 +0200

    deployment works successfully

--- Kobtan-api/Kobtan/views.py
+++ Kobtan-api/Kobtan/views.py
@@ -1,3 +1,3 @@
 from django.http import HttpResponse
 def homePageView(request):
-  return HttpResponse('Hello, World!')
+  return HttpResponse('Kobtan Api running successfully, please go to /api/ to see api endpoints,and go to /admin/ if you want got to admin page but you need username and password contact with Abdulmohaimen Elbuni to give you credential')
